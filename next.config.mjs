62682d324694b85bdd925aa822fa050d2f20ce89
yangkun <yangkun@taou.com>
2024-06-28 18:13:43 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
-  // output: "export",
+  output: "export",
   images: { unoptimized: true },
 };
 
