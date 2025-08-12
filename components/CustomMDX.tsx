54740c97b51ce5bac0db463ff0f2a1e682a083b7
kunsun <yangkunyjw@qq.com>
2025-08-12 15:54:35 +0800

    feat: 移除不必要的组件和代码块，简化 CustomMDX 组件 chore: 添加 overnight 依赖

--- components/CustomMDX.tsx
+++ components/CustomMDX.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkSmartpants from "remark-smartypants";
 import rehypePrettyCode from "rehype-pretty-code";
@@ -9,30 +7,9 @@ import githubLight from "tm-themes/themes/github-light-default.json";
 import { remarkMdxEvalCodeBlock } from "./mdx.js";
 import React from "react";
 import { Tag } from "./Tag";
-import { EnhancedCodeBlock } from "./EnhancedCodeBlock";
 
 const components = {
-  h1: (props: any) => {
-    return <h1>{props.children}</h1>;
-  },
-  h2: (props: any) => {
-    return <h2>{props.children}</h2>;
-  },
-  // 让外部 markdown-body / .markdown 样式接管段落排版，避免颜色冲突
-  p: (props: any) => <p>{props.children}</p>,
-  code: (props: any) => {
-    return <code className="text-sm p-1">{props.children}</code>;
-  },
-  pre: (props: any) => {
-    // 如果 pre 包含 data-rehype-pretty-code-fragment，保持原样
-    if (props["data-rehype-pretty-code-fragment"] !== undefined) {
-      return <pre {...props} />;
-    }
-    // 否则使用增强的代码块
-    return <EnhancedCodeBlock>{props.children}</EnhancedCodeBlock>;
-  },
   Tag,
-  EnhancedCodeBlock,
 };
 
 export function CustomMDX({ filename, source, postComponents }: any) {
