ce1126fe24e3db47f91e940d3c3b6688fcacd23e
kunsun <yangkunyjw@qq.com>
2025-08-12 15:28:37 +0800

    chore: update TypeScript target to ES2017 in tsconfig.json

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,17 +4,19 @@ import { readdir, readFile } from "fs/promises";
 import matter from "gray-matter";
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  const filename = `./posts/${params.slug}/index.mdx`;
+  const { slug } = await params;
+  const filename = `./posts/${slug}/index.mdx`;
   const content = await readFile(filename, "utf-8");
   let postComponents = {};
   try {
-    postComponents = await import("@posts/" + params.slug + "/components.js");
+    postComponents = await import("@posts/" + slug + "/components.js");
   } catch (e: any) {
     if (!e || e.code !== "MODULE_NOT_FOUND") {
       throw e;
     }
   }
   const { data, content: mdxContent } = matter(content);
+
   return (
     <article className="font-mono w-full min-w-0">
       <h1>{data.title}</h1>
